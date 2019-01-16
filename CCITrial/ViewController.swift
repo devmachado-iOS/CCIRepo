9dfb8667916fd1f1c8ee7411ea2b67cbf83cfbdf
carloshm <carlos.hv.machado@icloud.com>
2019-01-16 16:52:09 -0400

    First commit

---
+++ CCITrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CCITrial
+//
+//  Created by Carlos Henrique on 16/01/2019.
+//  Copyright © 2019 Carlos Henrique. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
